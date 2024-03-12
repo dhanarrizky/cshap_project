eeeb91f3687fea79096beb171f8dabba1ab5f420
dhanarrizky <dhanarrizky23@gmail.com>
2024-03-12 16:29:40 +0700

    createing project using api getway

---
+++ ApiGetWay/src/product/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace product.Controllers;
+
+[ApiController]
+[Route("Product")]
+public class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    public ProductController(ILogger<ProductController> logger){
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult ProductGet(){
+        var res = new Response{
+            StatusCode = 200,
+            Message = "Get Product Api has been successfully"
+        };
+        return Ok(res);
+    }
+}
+
+
+public class Response {
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = null!;
+}
---
+++ ApiGetWay/src/seller/Controllers/SellerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace seller.Controllers;
+
+[ApiController]
+[Route("Seller")]
+public class SellerController : ControllerBase
+{
+    private readonly ILogger<SellerController> _logger;
+    public SellerController(ILogger<SellerController> logger){
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult SellerGet(){
+        var res = new Response {
+            StatusCode = 200,
+            Message = "Get Seller Api has been successfully"
+        };
+
+        return Ok(res);
+    }
+}
+
+public class Response {
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = null!;
+}
