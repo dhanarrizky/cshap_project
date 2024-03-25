017f0269cae480d1970b71b66510b85e77fdb666
dhanarrizky <dhanarrizky23@gmail.com>
2024-03-25 14:23:17 +0700

    updated code for faker checker mail

--- ChatApp/ServerChat/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ServerChat;
-public class UserModel
-{
-    [Required]
-    [StringLength(15, MinimumLength = 3, ErrorMessage = "Name must be at least (2), and maximum (1) characters")]
-    public string Name { get; set; } = null!;
-}
---
+++ FakeMailCheker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+namespace FakeMailChecker;
+
+public class Program {
+    public static async Task Main(string[]args){
+        Console.WriteLine(await CheckThisMail("dhanar.krisnadhy@gmail.com"));
+    }
+
+    public static async Task<string> CheckThisMail(string emailAdd){
+        string[] splitMail = emailAdd.Split('@');
+        string url = "https://" + ((string)splitMail[1]);
+        Console.WriteLine(url);
+        try {
+            using HttpClient hc = new HttpClient();
+            HttpResponseMessage response = await hc.GetAsync(url);
+
+            if(response.IsSuccessStatusCode) {
+                return $"email is true, this is not a fake email....";
+            } else {
+                return $"Failed to fetch url. Status code : {response.StatusCode}";
+            }
+        } catch {
+            return "wrong email, this is a fake email";
+        }
+    }
+}
